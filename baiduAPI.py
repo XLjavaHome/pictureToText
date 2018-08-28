604ff09e8cf92e894977e1e8498c90b4e409233c
xuli <78645574@qq.com>
2018-08-28 17:44:12 +0800

    去掉两端空格

--- baiduAPI.py
+++ baiduAPI.py
@@ -1,19 +1,14 @@
 from aip import AipOcr
-
 """ 你的 APPID AK SK """
 APP_ID = '11695638'
 API_KEY = 'MFO5aHQ4IL37Q5ghIiSSnRZ0'
 SECRET_KEY = 'LOUz72LeeLGb9Y8kt2F64jRVibEtQrIY'
 client = AipOcr(APP_ID, API_KEY, SECRET_KEY)
 """ 读取图片 """
-
-
 def get_file_content(filePath):
     # r读 b二进制
     with open(filePath, 'rb') as fp:
         return fp.read()
-
-
 def pictrueToText(pictruePath):
     image = get_file_content(pictruePath)
     """ 调用通用文字识别, 图片参数为本地图片 """
@@ -21,5 +16,8 @@ def pictrueToText(pictruePath):
     # 字典和列表
     allText = ''
     for word in texts['words_result']:
-        allText = allText + "\n" + word.get('words', '')
+        text = word.get('words', '')
+        # 去掉两端空格
+        if text.strip() != '':
+            allText = allText + "\n" + text
     return allText
--- keyboardDemo.py
+++ keyboardDemo.py
@@ -8,10 +8,11 @@ import pyperclip
 from PIL import ImageGrab
 
 import baiduAPI
-
+# 一直循环相当于while true
 for n in range(sys.maxsize):
     # 截图开始,不区分大小写
-    if keyboard.wait('Ctrl+alt+a') == None:
+    # 加上没有用or keyboard.wait('shift+alt+a') == None
+    if keyboard.wait('shift+alt+a') == None:
         # 截图结束 TODO 监听鼠标左键,目前只搜到python2的
         if keyboard.wait('enter') == None:
             time.sleep(0.01)
@@ -26,4 +27,3 @@ for n in range(sys.maxsize):
                 # 将内容复制进剪切板
                 pyperclip.copy(result)
                 print(result)
-                print('内容在剪切板中')
