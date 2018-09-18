e75f90f9e06c34a54e12c68146516d7f565aa49d
徐立 <78645574@qq.com>
2018-09-18 23:02:41 +0800

    init

--- keyboardDemo.py
+++ keyboardDemo.py
@@ -1,6 +1,5 @@
 # 键盘
 import os
-import sys
 # 截图
 import time
 
@@ -11,10 +10,11 @@ from PIL import ImageGrab
 import baiduAPI
 
 # 一直循环相当于while true
-for n in range(sys.maxsize):
+# for n in range(sys.maxsize):
+while True:
     # 截图开始,不区分大小写
     # 加上没有用or keyboard.wait('shift+alt+a') == None
-    if keyboard.wait('alt+ctrl+a') == None:
+    if keyboard.wait('ctrl+alt+a') == None:
         # 截图结束 TODO 监听鼠标左键,目前只搜到python2的
         if keyboard.wait('enter') == None:
             time.sleep(0.01)
@@ -24,7 +24,6 @@ for n in range(sys.maxsize):
                 print("无法识别该图片")
             else:
                 picturePath = os.path.join(os.getcwd(), picturePath)
-                # os.chmod(picturePath, os.stat.S_IWRITE)
                 im.save(picturePath)
                 # 识别图片
                 result = baiduAPI.pictrueToText(picturePath)
