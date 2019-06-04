a49f3d77c7e3e4e612441be35969d1631bd57614
xuli <78645574@qq.com>
2019-06-04 17:12:16 +0800

    init

--- keyboardDemo2.py
+++ keyboardDemo2.py
@@ -1,10 +1,16 @@
 import keyboard
 import pinyin
 import pyperclip
+def is_Chinese(ch):
+    if '\u4e00' <= ch <= '\u9fff':
+        return True
+    return False
 while True:
     if keyboard.wait('ctrl+alt+p') == None:
         # 读取剪切板中的文字，并获取每个文字的首字母，复制进剪切板
         paste = pyperclip.paste()
-        result = pinyin.get_initial(paste).replace(' ', '')
-        pyperclip.copy(result+paste)
-        print(result)
+        # 以字母开头不用复制
+        if is_Chinese(paste[0]):
+            result = pinyin.get_initial(paste).replace(' ', '')
+            pyperclip.copy(result + paste)
+            print(result)
